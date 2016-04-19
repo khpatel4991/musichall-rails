50dfb30308d0d300d0fa11affa81946d4746fd61
Kashyap Patel <khpatel4991@gmail.com>
2016-04-19 02:10:31 -0400

    Added authenticaticate_user method for future use

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def authenticate_user
+    #TODO Remember the path if current_user is nil,
+    # so that after successful login, we can pass him off to the main page he wanted to go
+    redirect_to login_url, :alert => 'Please Login' if @current_user.nil?
+  end
+
   helper_method :current_user
 
 
